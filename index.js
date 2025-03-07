32aba52dd629b98eff8a5d2e54778f9aebe705f3
Sergei1908 <gaalsergei@gmail.com>
2025-03-07 19:06:45 +0100

    block plus and minus js

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const resultBlock = document.querySelector("#result");
 const counterAdd = document.getElementById("button-plus");
 
 
+
 let number = 0;
 
 resultBlock.textContent = number;
-counterAdd.addEventListener("click", () => {
-  number++;
-  counterAdd.textContent = number;
 
-});
+const counterPlus = () => {
+  number++;
+  resultBlock.textContent = number;
+};
+counterAdd.addEventListener("click",counterPlus)
 const counterMinus = () => {
   number--;
   resultBlock.textContent = number;
