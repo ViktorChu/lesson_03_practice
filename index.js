ce9160723951f922e3bae37944dd786429d48109
Sergei1908 <gaalsergei@gmail.com>
2025-03-07 18:56:56 +0100

    block js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const buttonMinus = document.querySelector("#button-minus");
 const resultBlock = document.querySelector("#result");
+const counterAdd = document.getElementById("button-plus");
+
 
 let number = 0;
-resultBlock.textContent = number;
 
+resultBlock.textContent = number;
+counterAdd.addEventListener("click", () => {
+  number++;
+  counterAdd.textContent = number;
+
+});
 const counterMinus = () => {
   number--;
   resultBlock.textContent = number;
